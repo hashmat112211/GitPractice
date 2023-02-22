2732c00c6627e72091acbebfa70f4f9937a8f43d
Hashmatullah Asad <hashmatullahasad@hashmatllahsmbp.myfiosgateway.com>
2023-02-22 18:12:14 -0500

    push code to repo

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -4,8 +4,9 @@ public class GitTest {
 
 	public void test() {
 		System.out.println("this is just a test");
-		
-		"hello"
+		///ystem.out.println("this is just a test");
+		//ystem.out.println("this is just a test");
+		//"hello"
 		
 		//Push code to repository 
 		
